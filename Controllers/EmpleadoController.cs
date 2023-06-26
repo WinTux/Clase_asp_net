1b5b775ce1cfd09fa5b3e72b879143d20101d7b4
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-06-26 19:51:31 -0400

    Listas controlador -> vista & appsettings.json

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Clase_asp_net.Controllers
 {
+    [Route("empleado")]
     public class EmpleadoController : Controller
     {
+        private IConfiguration configuration;
+        public EmpleadoController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [Route("index")]
+        [Route("")]
         public IActionResult Index()
         {
             // proceso 1
@@ -27,7 +35,45 @@ namespace Clase_asp_net.Controllers
             };
             ViewBag.empleado = emp;
 
+            //Pasandole un conjunto de objetos Empleado
+            var emps = new List<Empreado> {
+                new Empreado {
+                    ci = 3302,
+                    nombre = "ana Sosa",
+                    estatura = 1.70,
+                    peso = 68,
+                    foto = "uno.jpeg"
+                },
+                new Empreado {
+                    ci = 3303,
+                    nombre = "Sofía Rocha",
+                    estatura = 1.78,
+                    peso = 60,
+                    foto = "dos.jpeg"
+                },
+                new Empreado {
+                    ci = 3304,
+                    nombre = "Carlos Ballarta",
+                    estatura = 1.68,
+                    peso = 50,
+                    foto = "tres.jpeg"
+                }
+            };
+            ViewBag.empleados = emps;
+
             return View();
+        }
+        [Route("detalles")]
+        [Route("detallazos")]
+        public IActionResult Ejemplo1() {
+            ViewBag.variable1 = configuration["Texto"];
+            ViewBag.variable2 = configuration["VariablesConfig:var1"];
+            ViewBag.variable3 = configuration["VariablesConfig:var2"];
+            ViewBag.variable4 = configuration["VariablesConfig:var3"];
+            ViewBag.variable5 = configuration["VariablesConfig:varx:default"];
+            ViewBag.variable6 = configuration["VariablesConfig:varx:advanced:extremo"];
+
+            return View("Configuraciones");
         }
     }
 }
