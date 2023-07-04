dca2a1fd48a872add191d7c0bfac58444da0154c
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-07-04 19:11:53 -0400

    Sesiones 2

--- Controllers/EjemploSesionController.cs
+++ Controllers/EjemploSesionController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clase_asp_net.Extra;
+using Clase_asp_net.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clase_asp_net.Controllers
 {
@@ -6,6 +8,37 @@ namespace Clase_asp_net.Controllers
     {
         public IActionResult Index()
         {
+            HttpContext.Session.SetInt32("edad", 21);
+            HttpContext.Session.SetString("nombre", "Pepe");
+
+            Producto producto = new Producto { 
+                id = "prod01",
+                nombre = "Atún",
+                precio = 10.5
+            };
+
+            ConversorParaSesion.ConvertirAjson(HttpContext.Session, "prod", producto);
+
+            List<Producto> productos = new List<Producto>()
+            {
+                new Producto {
+                id = "prod01",
+                nombre = "Queso",
+                precio = 15
+                },
+                new Producto {
+                id = "prod02",
+                nombre = "Atún",
+                precio = 10.5
+                },
+                new Producto {
+                id = "prod03",
+                nombre = "Arroz",
+                precio = 19.2
+            }
+            };
+            ConversorParaSesion.ConvertirAjson(HttpContext.Session, "prods", productos);
+
             return View();
         }
     }
