8817fa879a06712e4b6e765af7c34b931e4df6d0
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-07-06 19:26:36 -0400

    Layouts 1

---
+++ Controllers/EjemploLayoutsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase_asp_net.Controllers
+{
+    public class EjemploLayoutsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Destino1()
+        {
+            return View();
+        }
+        public IActionResult Destino2()
+        {
+            return View();
+        }
+    }
+}
